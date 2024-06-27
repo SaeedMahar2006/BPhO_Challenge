ee415d35f3de6b62ad6e059d58dcbf68e8139b48
SaeedMahar2006 <SaeedMahar2006@gmail.com>
2024-06-27 14:25:24 +0100

    first commit, codestral comments removed

--- Core/SimpleFormulae.cs
+++ Core/SimpleFormulae.cs
@@ -67,44 +67,18 @@ namespace Core
         }
 
 
-    /// <summary>
-    /// Calculates the minimum initial speed required for a projectile to reach a specific point (ThroughX, ThroughY)
-    /// under the influence of gravity (Gravity).
-    /// </summary>
-    /// <param name="Gravity">The acceleration due to gravity (m/s²).</param>
-    /// <param name="ThroughX">The horizontal distance to the target point (m).</param>
-    /// <param name="ThroughY">The vertical distance to the target point (m).</param>
-    /// <returns>The minimum initial speed required to reach the target point (m/s).</returns>
+
         public static double ThroughPointMinSpeed(double Gravity, double ThroughX, double ThroughY)
         {
             return Math.Sqrt(Gravity * (ThroughY + Math.Sqrt(ThroughX * ThroughX + ThroughY * ThroughY)));
         }
 
-            /// <summary>
-    /// Calculates the angle at which the projectile must be launched to reach a specific point (ThroughX, ThroughY)
-    /// with the minimum initial speed, under the influence of gravity (Gravity).
-    /// </summary>
-    /// <param name="Gravity">The acceleration due to gravity (m/s²).</param>
-    /// <param name="ThroughX">The horizontal distance to the target point (m).</param>
-    /// <param name="ThroughY">The vertical distance to the target point (m).</param>
-    /// <returns>The angle at which the projectile must be launched to reach the target point with the minimum initial speed (radians).</returns>
         public static double ThroughPointMinSpeedAngle(double Gravity, double ThroughX, double ThroughY)
         {
             return Math.Atan((ThroughY + Math.Sqrt(ThroughX * ThroughX + ThroughY * ThroughY)) / ThroughX);
         }
 
-            /// <summary>
-    /// Calculates the angle at which the projectile must be launched to reach a specific point (ThroughX, ThroughY)
-    /// with a given initial speed (LaunchSpeed) and launch height (LaunchHeight), under the influence of gravity (Gravity).
-    /// This method returns the lower angle of the two possible angles.
-    /// </summary>
-    /// <param name="Gravity">The acceleration due to gravity (m/s²).</param>
-    /// <param name="LaunchSpeed">The initial speed of the projectile (m/s).</param>
-    /// <param name="ThroughX">The horizontal distance to the target point (m).</param>
-    /// <param name="ThroughY">The vertical distance to the target point (m).</param>
-    /// <param name="LaunchHeight">The height from which the projectile is launched (m). Default is 0.</param>
-    /// <returns>The lower angle at which the projectile must be launched to reach the target point with the given initial speed (radians).
-    /// Returns NaN if there is no solution.</returns>
+
 
         public static double ThroughPointLowBallAngle(double Gravity, double LaunchSpeed, double ThroughX, double ThroughY, double LaunchHeight = 0)
         {
@@ -116,18 +90,7 @@ namespace Core
             return Math.Atan((-b - Math.Sqrt(discriminant)) / (2 * a));
         }
 
-            /// <summary>
-    /// Calculates the angle at which the projectile must be launched to reach a specific point (ThroughX, ThroughY)
-    /// with a given initial speed (LaunchSpeed) and launch height (LaunchHeight), under the influence of gravity (Gravity).
-    /// This method returns the larger angle of the two possible angles.
-    /// </summary>
-    /// <param name="Gravity">The acceleration due to gravity (m/s²).</param>
-    /// <param name="LaunchSpeed">The initial speed of the projectile (m/s).</param>
-    /// <param name="ThroughX">The horizontal distance to the target point (m).</param>
-    /// <param name="ThroughY">The vertical distance to the target point (m).</param>
-    /// <param name="LaunchHeight">The height from which the projectile is launched (m). Default is 0.</param>
-    /// <returns>The lower angle at which the projectile must be launched to reach the target point with the given initial speed (radians).
-    /// Returns NaN if there is no solution.</returns>
+
         public static double ThroughPointHighBallAngle(double Gravity, double LaunchSpeed, double ThroughX, double ThroughY, double LaunchHeight = 0)
         {
             double a = Gravity * ThroughX * ThroughX / (2 * LaunchSpeed * LaunchSpeed);
@@ -141,25 +104,13 @@ namespace Core
 
 
 
-/// <summary>
-/// Calculates the maximum range of a projectile given the acceleration due to gravity, launch speed, and launch height.
-/// </summary>
-/// <param name="Gravity">The acceleration due to gravity.</param>
-/// <param name="LaunchSpeed">The initial launch speed of the projectile.</param>
-/// <param name="LaunchHeight">The initial launch height of the projectile.</param>
-/// <returns>The maximum range of the projectile.</returns>
+
         public static double MaxRange(double Gravity, double LaunchSpeed, double LaunchHeight)
         {
             return AnalyticNoDragRange(MaxRangeAngle(Gravity, LaunchSpeed, LaunchHeight), Gravity, LaunchSpeed, LaunchHeight);
         }
 
-        /// <summary>
-/// Calculates the angle of launch that results in the maximum range of a projectile given the acceleration due to gravity, launch speed, and launch height.
-/// </summary>
-/// <param name="Gravity">The acceleration due to gravity.</param>
-/// <param name="LaunchSpeed">The initial launch speed of the projectile.</param>
-/// <param name="LaunchHeight">The initial launch height of the projectile.</param>
-/// <returns>The angle of launch that results in the maximum range of the projectile.</returns>
+
         public static double MaxRangeAngle(double Gravity, double LaunchSpeed, double LaunchHeight)
         {
             double alpha = 2 * Gravity * LaunchHeight / (LaunchSpeed * LaunchSpeed);
@@ -167,14 +118,6 @@ namespace Core
         }
 
 
-/// <summary>
-/// Calculates the bounding height of a projectile at a given horizontal distance.
-/// </summary>
-/// <param name="x">The horizontal distance from the launch point.</param>
-/// <param name="Gravity">The acceleration due to gravity.</param>
-/// <param name="LaunchSpeed">The initial launch speed of the projectile.</param>
-/// <param name="LaunchHeight">The initial launch height of the projectile.</param>
-/// <returns>The bounding height of the projectile at the given horizontal distance.</returns>
         public static double BoundingHeightAtDistanceX(double x, double Gravity, double LaunchSpeed, double LaunchHeight)
         {
             double alpha = (LaunchSpeed * LaunchSpeed) / (Gravity);
@@ -190,15 +133,6 @@ namespace Core
             return (Math.Log(Math.Sqrt(1 + x * x) + x) + x * Math.Sqrt(1 + x * x)) / 2;//x>=0 so always ok 
         }
 
-    /// <summary>
-    /// Calculates the length of a trajectory given the horizontal distance, angle of launch, gravity, launch speed, and launch height.
-    /// </summary>
-    /// <param name="X">The horizontal distance.</param>
-    /// <param name="Angle">The angle of launch in radians.</param>
-    /// <param name="Gravity">The acceleration due to gravity.</param>
-    /// <param name="LaunchSpeed">The initial launch speed.</param>
-    /// <param name="LaunchHeight">The initial launch height.</param>
-    /// <returns>The length of the trajectory.</returns>
         public static double LengthOfTrajectory(double X, double Angle, double Gravity, double LaunchSpeed, double LaunchHeight)
         {
             double multiplier = LaunchSpeed*LaunchSpeed*Math.Pow(Math.Cos(Angle),2)/Gravity;
